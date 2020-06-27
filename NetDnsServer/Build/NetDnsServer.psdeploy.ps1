de1a60b36db9b8d2b3bdf97808d5bf2ce01b018d
Greg Powers <gpowers@tdev.local>
2020-06-27 14:08:26 -0400

    asdsadsadas

--- NetDnsServer/Build/NetDnsServer.psdeploy.ps1
+++
@@ -1,9 +0,0 @@
-﻿Deploy PStdev {
-    By PSGalleryModule {
-        FromSource $PSScriptRoot\..\NetDnsServer
-        To PStdev
-        WithOptions @{
-            ApiKey = "InbJe8TKTerNDuUnIeW5" # <-- API Key from ProGet
-        }
-    }
-}
