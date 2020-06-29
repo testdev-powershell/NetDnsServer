55d47616fd358390e69e3a9ce9ee4790c2f08a20
Greg Powers <testdev.gpowers@gmail.com>
2020-06-29 09:37:42 -0400

    asdasd

--- Build/NetDnsServer_Build.ps1
+++ Build/NetDnsServer_Build.ps1
@@ -8,19 +8,6 @@ $Task = 'default'
 
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-<<<<<<< HEAD
-if (!(Get-Module -Name Pester -ListAvailable)) {
-    Install-Module -Name Pester -Scope CurrentUser -Force
-}
-if (!(Get-Module -Name psake -ListAvailable)) {
-    Install-Module -Name psake -Scope CurrentUser -Force
-}
-if (!(Get-Module -Name PSDeploy -ListAvailable)) {
-    Install-Module -Name PSDeploy -Scope CurrentUser -Force
-}
-
-=======
->>>>>>> testDEV
 Invoke-psake -buildFile "$PSScriptRoot\NetDnsServer_psakeBuild.ps1" -taskList $Task -Verbose:$VerbosePreference
 
 if ($psake.build_success -eq $false) {
