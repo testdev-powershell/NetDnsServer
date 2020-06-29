00655c010d8af4e2388c126efdf3f66b7fc75ba8
Greg Powers <gpowers@tdev.local>
2020-06-29 12:13:46 -0400

    asdasd

--- Helpers/ModuleHelpers.ps1
+++ Helpers/ModuleHelpers.ps1
@@ -16,6 +16,14 @@ if (!(Get-InstalledModule -Name BuildHelpers -ErrorAction Ignore)) {
     Install-Module -Name BuildHelpers -Scope CurrentUser -Force
 }
 
+if (!(Get-PSRepository -Name PStdev)) { \
+    Register-PSRepository -Name PStdev -SourceLocation 'http://192.168.1.211:8624/nuget/PStdev/' -PublishLocation 'http://192.168.1.211:8624/nuget/PStdev/' -InstallationPolicy Trusted \
+}
+
+if (!(Get-InstalledModule -Name NetDnsServer -ErrorAction Ignore)) {
+    Install-Module -Name NetDnsServer -Repository PStdev -Scope CurrentUser -Force
+}
+
 if (!(Get-InstalledModule -Name Pester -ErrorAction Ignore)) {
     Install-Module -Name Pester -Scope CurrentUser -Force -SkipPublisherCheck
 }
